64cdb31e78b11741ffbb4deec504a2a8bd157270
Ismailbulbul21 <ismailbulbul381@gmail.com>
2024-11-08 16:37:45 +0300

    changes mobile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import HomePage from './pages/HomePage';
@@ -26,10 +26,17 @@ function App() {
             <Header />
             <main className="flex-grow">
               <Routes>
-                <Route path="/" element={<HomePage />} />
+                <Route path="/" element={<Navigate to="/products" replace />} />
                 <Route path="/products" element={<ProductsPage />} />
                 <Route path="/product/:id" element={<ProductDetailsPage />} />
-                <Route path="/cart" element={<CartPage />} />
+                <Route
+                  path="/cart"
+                  element={
+                    <PrivateRoute>
+                      <CartPage />
+                    </PrivateRoute>
+                  }
+                />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/signup" element={<SignupPage />} />
                 <Route path="/contact" element={<ContactPage />} />
@@ -57,6 +64,7 @@ function App() {
                     </PrivateRoute>
                   }
                 />
+                <Route path="*" element={<Navigate to="/products" replace />} />
               </Routes>
             </main>
             <Footer />
